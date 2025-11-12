538c750c5a643525b1fd7f0b7937d5113e60157e
Beemil <valeskavr17@gmail.com>
2025-11-11 19:54:24 -0600

    Create types Descuento

---
+++ src/types/Descuento.ts
@@ -0,0 +1,11 @@
+export interface Descuento {
+    producto1: number;
+    producto2: number;
+    producto3: number;
+    producto4: number;
+    producto5: number;
+    porcentajeDescuento: number;
+    montoDescuento: number;
+    totalPagar: number;
+}
+export type NombreProducto = 'producto1'|'producto2'|'producto3'|'producto4'|'producto5';
